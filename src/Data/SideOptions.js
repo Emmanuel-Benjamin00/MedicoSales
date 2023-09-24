4eb91f3d05ff4a8f7c5eeba9edd72d955a5d5ca4
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-09-24 11:42:08 +0530

    Medico Sales deployment

---
+++ src/Data/SideOptions.js
@@ -0,0 +1,55 @@
+const SideOptions = [
+    {
+        menu: "Dashboard",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/Dashboard.svg",
+        activimg:"/images/side-icons/Dashboard Blue.svg",
+        isActive:true
+    },
+    {
+        menu: "Lab Test",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/Lab Test.svg",
+        activimg:"/images/side-icons/Lab Test Blue.svg",
+        isActive:false
+    },
+    {
+        menu: "Appointment",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/Appointment.svg",
+        activimg:"/images/side-icons/Appointment Blue.svg",
+        isActive:false
+    },
+    {
+        menu: "Medicine Order",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/Medicine Order.svg",
+        activimg:"/images/side-icons/Medicine Order Blue.svg",
+        isActive:false
+    },
+    {
+        menu: "Message",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/message.svg",
+        activimg:"/images/side-icons/message Blue.svg",
+        isActive:false
+    },
+    {
+        menu: "Payment",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/Payment.svg",
+        activimg:"/images/side-icons/Payment Blue.svg",
+        isActive:false
+    },
+    {
+        menu: "Settings",
+        activeline:"/images/side-icons/Active Side Blue Line.svg",
+        img:"/images/side-icons/settings.svg",
+        activimg:"/images/side-icons/settings Blue.svg",
+        isActive:false
+    },
+
+
+]
+
+export default SideOptions;
